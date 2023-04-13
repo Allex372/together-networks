068aad5e3325814afa7bce6db7a5e4ba10828db5
Allex372 <alexalex372.aa@gmail.com>
2023-04-13 16:56:21 +0300

    test

--- src/forms/Password.js
+++ src/forms/Password.js
@@ -1,6 +1,7 @@
 import { FormWrapper } from '../components';
 
 export function Password({ password, updateFields }) {
+    console.log(password);
     return (
         <FormWrapper title='Create your password'>
             <input type='password' value={password} onChange={e => updateFields({ password: e.target.value })} />
