d5ec0da14fdd9fc9b3e8d15c7fbf448847f86f10
Allex372 <alexalex372.aa@gmail.com>
2023-04-14 12:41:24 +0300

    fix

--- src/forms/Password.js
+++ src/forms/Password.js
@@ -1,7 +1,6 @@
 import { FormWrapper } from '../components';
 
 export function Password({ password, updateFields }) {
-    console.log(password);
     return (
         <FormWrapper title='Create your password'>
             <input type='password' value={password} onChange={e => updateFields({ password: e.target.value })} />
